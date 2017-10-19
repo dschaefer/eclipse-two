001f8c68890f56dc5e7a409c0ef22acc75852a05
Doug Schaefer <dschaefer@blackberry.com>
2017-10-19 16:17:08 -0400

    Update to latest upstreams.

--- src/core/main.js
+++ src/core/main.js
@@ -16,6 +16,7 @@ const url = require('url');
 let mainWindow;
 
 app.setName("Two IDE");
+app.commandLine.appendSwitch('ignore-gpu-blacklist');
 
 function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600});
